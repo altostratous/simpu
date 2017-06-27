b979e17aa7b4e8a79ba0323b114b4fae3f5cc4a1
RasoulAM <rasoul.am1376@gmail.com>
2017-06-27 16:45:02 +0430

    another commit

--- cpu/data_path/instmemory.v
+++ cpu/data_path/instmemory.v
@@ -4,7 +4,7 @@ module instmemory( write, addr, datain, dataout, clk, reset);
 	input write, clk, reset;
 	output reg[31:0] dataout;
 	
-	reg[31:0] mem[2**16 - 1 : 0];
+	reg[31:0] mem[65535 : 0];
 	
 	always @(posedge clk) begin
 		if(reset) begin
