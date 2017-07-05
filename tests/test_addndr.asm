4a9c160e47bffbdd6835feac3ef386da6368a785
altostratous <altostratous@gmail.com>
2017-07-05 08:12:34 +0430

    Tested ADDNDR

---
+++ tests/test_addndr.asm
@@ -0,0 +1,8 @@
+start           ORI     $1  $1  5
+                ORI     $2  $1  2
+                SW      $1  0
+                SW      $2  1
+                LUI     $1  0
+                ORI     $2  $1  1
+                ADDNDR  $3  $2  $1
+                SW      $3  2
